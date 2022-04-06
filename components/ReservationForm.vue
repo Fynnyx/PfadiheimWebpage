7c65bda96fda9484fbe9a08c99bceefc1f809276
Fynnyx <westerathf@gmail.com>
2022-04-06 16:17:19 +0200

    reservation form file

---
+++ components/ReservationForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="reservation-form">
+      <form action="#">
+          <input type="text">
+          
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
