7617f8e0975cbd01a5b1774b0a5131b46aa95ba0
Fynnyx <westerathf@gmail.com>
2022-04-12 06:37:02 +0200

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,27 @@
 <template>
-    
+  <div class="footer">
+    <span> Copyright by Pfadiheimverein Baden</span>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "Footer",
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  background: $black-90;
+  > span {
+      font-size: 12px;
+  }
+}
 </style>
